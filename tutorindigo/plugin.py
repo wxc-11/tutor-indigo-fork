041a87e729b34c5cfa932c376326fa24e22d42ff
wxc-11 <55144626+wxc-11@users.noreply.github.com>
2022-10-28 11:25:03 +0800

    Update plugin.py

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -38,12 +38,12 @@ hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
 # This is where the theme is rendered in the openedx build directory
 hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     [
-        ("indigo", "build/openedx/themes"),
+        ("indigo-growgreen", "build/openedx/themes"),
     ],
 )
 
 # Force the rendering of scss files, even though they are included in a "partials" directory
-hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(r"indigo/lms/static/sass/partials/lms/theme/")
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(r"indigo-growgreen/lms/static/sass/partials/lms/theme/")
 
 # Load all configuration entries
 hooks.Filters.CONFIG_DEFAULTS.add_items(
